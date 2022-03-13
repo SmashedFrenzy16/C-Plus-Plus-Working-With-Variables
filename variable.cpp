aa748a48cfe2b50f09a7bad81ecf6b7863d731be
SmashedFrenzy16 <68993968+SmashedFrenzy16@users.noreply.github.com>
2022-03-13 20:12:19 +0000

    Create variable.cpp

---
+++ variable.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  string name = "SmashedFrenzy16";
+  int year = 2022;
+  double piToTwoDecimalPlaces
+  cout << "Name: " << name << endl;
+  cout << "Year: " << year << endl;
+  count << "Pi to 2dp: " << piToTwoDecimalPlaces << endl;
+  return 0;
+}
